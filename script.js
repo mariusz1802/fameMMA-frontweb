147dbadd3358249d225bdfea9126c92617540cdb
Mariusz <mariusz.janowski@yahoo.pl>
2023-01-24 09:42:15 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const clock = document.querySelector('.clock');
+
+let interval;
+
+
+function startClock() {
+const data = new Date();
+let hours = data.getHours();
+let minutes = data.getMinutes();
+let seconds = data.getSeconds();
+
+
+if(hours < 9){
+    hours = `0${hours}`
+}
+if(minutes < 9){
+    minutes = `0${minutes}`
+}
+if(seconds < 9){
+    seconds = `0${seconds}`
+}
+
+if(hours > 9){
+
+    clock.innerHTML = `0${hours}: ${minutes}: ${seconds}`
+}
+
+clock.innerHTML = `${hours}: ${minutes}: ${seconds}`
+}
+
+
+ interval = setInterval(startClock, 1000);
+
+
