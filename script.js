decb4835cb108cdf2411a6fbc3b30493ee9093ff
Mariusz <mariusz.janowski@yahoo.pl>
2023-01-25 07:03:48 +0100

    FrontEnd changes

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-const clock = document.querySelector('.clock');
+// const clock = document.querySelector('.clock');
 
-let interval;
-
-
-function startClock() {
-const data = new Date();
-let hours = data.getHours();
-let minutes = data.getMinutes();
-let seconds = data.getSeconds();
+// let interval;
 
 
-if(hours < 9){
-    hours = `0${hours}`
-}
-if(minutes < 9){
-    minutes = `0${minutes}`
-}
-if(seconds < 9){
-    seconds = `0${seconds}`
-}
-
-if(hours > 9){
-
-    clock.innerHTML = `0${hours}: ${minutes}: ${seconds}`
-}
-
-clock.innerHTML = `${hours}: ${minutes}: ${seconds}`
-}
+// function startClock() {
+// const data = new Date();
+// let hours = data.getHours();
+// let minutes = data.getMinutes();
+// let seconds = data.getSeconds();
 
 
- interval = setInterval(startClock, 1000);
+// if(hours < 9){
+//     hours = `0${hours}`
+// }
+// if(minutes < 9){
+//     minutes = `0${minutes}`
+// }
+// if(seconds < 9){
+//     seconds = `0${seconds}`
+// }
+
+// if(hours > 9){
+
+//     clock.innerHTML = `0${hours}: ${minutes}: ${seconds}`
+// }
+
+// clock.innerHTML = `${hours}: ${minutes}: ${seconds}`
+// }
+
+
+//  interval = setInterval(startClock, 1000);
 
 
