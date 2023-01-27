9dc7cbab1f610a6d0097f291f47d3d403d32a3f1
Mariusz <mariusz.janowski@yahoo.pl>
2023-01-27 21:38:22 +0100

    countdown init funcitons

--- script.js
+++ script.js
@@ -4,26 +4,60 @@ let interval;
 
 function startClock() {
   const showTime = new Date("February 3, 2023 19:30:00");
-  const actualTime = new Date();
-
   const countToShow = showTime.getTime();
+  return countToShow;
 
-  console.log(countToShow);
 }
+
+const montsArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "Dezember"];
+
 
 const actualTimeCreator = () => {
   const date = new Date();
-  const day = date.getDay();
   const month = date.getMonth();
+  const day = date.getDay();
   const year = date.getFullYear();
 
   const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
 
-  console.log(date.getFullYear());
+  const stringMonth = montsArr[month];
+
+  const stringDate = `${stringMonth} ${day}, ${year} ${hours}:${minutes}:${seconds}`;
+  const actualDate = new Date(stringDate).getTime();
+  return actualDate
+
 };
 
-actualTimeCreator();
+function compareDates(){
+  
+  const day = 24 * 60 * 60 * 1000;
+  const hour = 60 * 60 * 1000;
+  const minute = 60 + 1000;
+  const second = 1*1000;
 
-interval = setInterval(startClock, 1000);
+  
+  const show = startClock();
+  const actual = new Date().getTime();
+
+  const timeLeft  = show - actual
+
+  const daysLeft = timeLeft / day;
+  const hoursLeft = timeLeft / hour;
+  const minutesLeft = timeLeft / minute;
+  const secondsLeft = timeLeft / second
+
+
+
+
+
+}
+
+
+
+
+
+
+
+interval = setInterval( compareDates, 1000);
